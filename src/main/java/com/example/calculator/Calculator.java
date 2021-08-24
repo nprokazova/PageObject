4a485eacb578f6a40f52965140e9f4a731d3ea33
nprokazova <naprokazova@gmail.com>
2021-08-25 00:00:51 +0300

    ref

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -2,19 +2,19 @@ package com.example.calculator;
 
 public class Calculator {
 
-    public Integer sum(Integer val1, Integer val2){
+    public Integer sum(Integer val1, Integer val2) {
         return val1 + val2;
     }
 
-    public Integer substraction(Integer val1, Integer val2){
+    public Integer substraction(Integer val1, Integer val2) {
         return val1 - val2;
     }
 
-    public Integer multiply(Integer val1, Integer val2){
+    public Integer multiply(Integer val1, Integer val2) {
         return val1 * val2;
     }
 
-    public Integer division(Integer val1, Integer val2){
+    public Integer division(Integer val1, Integer val2) {
         return val1 / val2;
     }
 
--- src/test/java/com/example/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/example/calculator/CalculatorApplicationTests.java
@@ -8,50 +8,51 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CalculatorApplicationTests {
 
-	private Calculator calculator;
+    private Calculator calculator;
 
-	@BeforeEach
-	public void setUp(){
-		calculator = new Calculator();
-	}
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
 
-	@Test
-	@DisplayName("sum")
-	void test1() {
-		Integer first = 3;
-		Integer second = 7;
-		Integer expected = 10;
+    @Test
+    @DisplayName("sum")
+    void test1() {
+        Integer first = 3;
+        Integer second = 7;
+        Integer expected = 10;
 
-		assert calculator.sum(first, second).equals(expected);
-	}
-	@Test
-	@DisplayName("sub")
-	void test2() {
-		Integer first = 3;
-		Integer second = 7;
-		Integer expected = -4;
+        assert calculator.sum(first, second).equals(expected);
+    }
 
-		assert calculator.substraction(first, second).equals(expected);
-	}
+    @Test
+    @DisplayName("sub")
+    void test2() {
+        Integer first = 3;
+        Integer second = 7;
+        Integer expected = -4;
 
-
-	@Test
-	@DisplayName("mul")
-	void test3() {
-		Integer first = 3;
-		Integer second = 7;
-		Integer expected = 21;
-
-		assert calculator.multiply(first, second).equals(expected);
-	}
+        assert calculator.substraction(first, second).equals(expected);
+    }
 
 
-	@Test
-	@DisplayName("div")
-	void tes41() {
-		Integer first = 3;
-		Integer second = 7;
-		Integer expected = 0;
+    @Test
+    @DisplayName("mul")
+    void test3() {
+        Integer first = 3;
+        Integer second = 7;
+        Integer expected = 21;
+
+        assert calculator.multiply(first, second).equals(expected);
+    }
+
+
+    @Test
+    @DisplayName("div")
+    void tes41() {
+        Integer first = 3;
+        Integer second = 7;
+        Integer expected = 0;
 
         assert calculator.division(first, second).equals(expected);
     }
